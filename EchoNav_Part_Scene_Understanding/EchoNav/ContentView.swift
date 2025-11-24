5cbacdb583f739e1211e3dbb3625dee146dd435d
Rayan Mazri <rayan.mazri@epitech.eu>
2025-11-23 18:07:23 -0800

    [ADD] Distance analyse with lidar

---
+++ EchoNav_Part_Scene_Understanding/EchoNav/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EchoNav
+//
+//  Created by Rayan Mazri on 23/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EchoNav_Part_Scene_Understanding/EchoNavTests/EchoNavTests.swift
@@ -0,0 +1,17 @@
+//
+//  EchoNavTests.swift
+//  EchoNavTests
+//
+//  Created by Rayan Mazri on 23/11/2025.
+//
+
+import Testing
+@testable import EchoNav
+
+struct EchoNavTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
